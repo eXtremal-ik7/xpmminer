4108edca5a0cd62b9693fbaa7452597b020f0356
Alexey Shvetsov <alexxy@gentoo.org>
2014-04-21 01:21:54 +0400

    Make it build on mesa opencl
    Signed-off-by: Alexey Shvetsov <alexxy@gentoo.org>

--- src/OpenCL/utils.cpp
+++ src/OpenCL/utils.cpp
@@ -87,7 +87,8 @@ int OpenCLInit(OpenCLPlatrormContext &ctx,
     {"GroupSize", 256},
     {"ExtensionsNum", extensionsNum},
     {"ChainLength", chainLength},
-    {"FixedRoundsNum", sieveSize / sieveWindowSize}
+    {"FixedRoundsNum", sieveSize / sieveWindowSize},
+    {"primesPerThread", weaveDepth/ 256}
   };
   
   std::string cmdLine;
@@ -98,7 +99,7 @@ int OpenCLInit(OpenCLPlatrormContext &ctx,
   cmdLine.push_back(' ');
   
   for (size_t i = 0; i < sizeof(arguments) / sizeof(cmdLineArg); i++) {
-    char buffer[16];
+    char buffer[20];
     snprintf(buffer, sizeof(buffer), "%u", arguments[i].value);
     cmdLine.append(" -D");
     cmdLine.append(arguments[i].name);
