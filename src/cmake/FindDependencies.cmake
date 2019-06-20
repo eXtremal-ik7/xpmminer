d6a2ce9898fbc827bd76f1eb59d731123bb7203b
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2019-06-20 16:24:51 +0300

    Found OpenSSL by cmake

--- src/cmake/FindDependencies.cmake
+++ src/cmake/FindDependencies.cmake
@@ -3,11 +3,6 @@ find_path(GMP_INCLUDE_DIRECTORY gmp.h gmpxx.h)
 find_library(GMP_LIBRARY gmp)
 find_library(GMPXX_LIBRARY gmpxx)
 
-# OpenSSL
-find_path(OPENSSL_INCLUDE_DIRECTORY openssl/bn.h)
-find_library(SSL_LIBRARY ssl)
-find_library(CRYPTO_LIBRARY crypto)
-
 # Jannson
 find_path(JANSSON_INCLUDE_DIRECTORY jansson.h)
 find_library(JANSSON_LIBRARY jansson)
